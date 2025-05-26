0be024336370bdbdb5a1cc9e5095f85c45ee731c
Milinda Hashan <165372276+Milindahashankumara@users.noreply.github.com>
2025-05-26 16:46:00 +0530

    create navbar and built ome page

---
+++ edusity/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+
+function App() {
+  return (
+    <>
+    <Navbar></Navbar>
+    <Hero></Hero>
+    
+    </>
+  )
+}
+
+export default App
---
+++ edusity/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Navbar.css';
+import logo from '../../assets/logo.png';
+
+function Navbar() {
+  return (
+    <div>
+        <nav className='container'>
+
+            <img src={logo} alt='' className='logo'/>
+            <ul>
+                <li>Home</li>
+                <li>Program</li>
+                <li>About us</li>
+                <li>Campus</li>
+                <li>Testimonials</li>
+                <li><button className='btn'>Contact us</button></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar
