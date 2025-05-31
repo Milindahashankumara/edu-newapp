2fd7771f9cceba414fe160879272426287df9e69
Milinda Hashan <165372276+Milindahashankumara@users.noreply.github.com>
2025-05-31 20:52:42 +0530

    create program feature

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
+import Programs from './Components/Programs/Programs';
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
     <Hero></Hero>
+    <div className='container'>
+        <Programs></Programs>
+    </div>
+    
     
     </>
   )
