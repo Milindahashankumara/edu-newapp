4807d5e12c6fddc3348c3bd12a18e3f5f2b07480
Milinda Hashan <165372276+Milindahashankumara@users.noreply.github.com>
2025-05-31 23:43:56 +0530

    create campus component

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero';
 import Programs from './Components/Programs/Programs';
 import Title from './Components/Title/Title';
 import About from './Components/About/About';
+import Campus from './Components/Campus/Campus';
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Title subTitle='Our Program'  title='What We Offer'/> 
         <Programs></Programs>
         <About></About>
+        <Title subTitle='Gallery'  title='Campus Photos'/> 
+        <Campus></Campus>
     </div>
     
     
