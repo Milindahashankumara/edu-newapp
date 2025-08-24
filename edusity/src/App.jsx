9a277c4e1c5800513def971781e2acc2c75d510f
Milindahashankumara <kumarawmmh.22@uom.lk>
2025-08-24 09:32:20 +0530

    create testimonials part

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import Navbar from './Components/Navbar/Navbar';
-import Hero from './Components/Hero/Hero';
-import Programs from './Components/Programs/Programs';
-import Title from './Components/Title/Title';
-import About from './Components/About/About';
-import Campus from './Components/Campus/Campus';
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Hero from "./Components/Hero/Hero";
+import Programs from "./Components/Programs/Programs";
+import Title from "./Components/Title/Title";
+import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
 
 function App() {
   return (
     <>
-    <Navbar></Navbar>
-    <Hero></Hero>
-    <div className='container'>
-        <Title subTitle='Our Program'  title='What We Offer'/> 
+      <Navbar></Navbar>
+      <Hero></Hero>
+      <div className="container">
+        <Title subTitle="Our Program" title="What We Offer" />
         <Programs></Programs>
         <About></About>
-        <Title subTitle='Gallery'  title='Campus Photos'/> 
+        <Title subTitle="Gallery" title="Campus Photos" />
         <Campus></Campus>
-    </div>
-    
-    
+        <Title subTitle="TESTIMONIALS" title="What Student Says" />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
