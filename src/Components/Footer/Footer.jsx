25ce7869c9b196575b8c00ac2c99496d192c9b7f
Milindahashankumara <kumarawmmh.22@uom.lk>
2025-08-26 18:41:38 +0530

    create footer part

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      <p> 2025 Edusity. All rights reserved.</p>
+      <ul>
+        <li>Term of Services</li>
+        <li>Privacy Policy</li>
+      </ul>
+    </div>
+  );
+}
